87f9fcd3c596bb900f6ddccaf0c346aaf5809fee
Lukasz <lukabrzo@gmail.com>
2025-08-28 00:35:50 +0100

    Initialised a setup for an API for the Express backend.

---
+++ maintenance-forms-app-prototype/server/backend-layer/app.js
@@ -0,0 +1,13 @@
+/*
+    This module sets up the Express application and middleware for the maintenance checks API.  
+    More details: https://expressjs.com/en/starter/installing.html
+*/
+
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
