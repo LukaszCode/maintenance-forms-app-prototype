87b7d826b94ca1a6521d06c3023aa2bd52738bc8
Lukasz <lukabrzo@gmail.com>
2025-08-28 22:05:19 +0100

    initialised the database connection and fixed the database structure

---
+++ maintenance-forms-app-prototype/maintenance_app/data/db/sqlite.ts
@@ -0,0 +1,30 @@
+/*
+    SQLite Database Connection
+    This module provides a connection to the SQLite database for the maintenance checks application.
+
+    Author: Lukasz Brzozowski
+    Created: 21/07/2025
+    Edited: 26/07/2025
+            30/07/2025
+            02/08/2025
+            14/08/2025
+*/
+
+import * as SQLite from "expo-sqlite";
+
+export async function openDatabase() {
+  const db = await SQLite.openDatabaseAsync("maintenance_checks.db");
+  await db.execAsync("PRAGMA foreign_keys=ON;");
+
+  await migrate(db);
+  return db;
+}
+
+async function migrate(db: SQLite.SQLiteDatabase) {
+  const [user_version] = await db.getFirstAsync("PRAGMA user_version;");
+  if (user_version === 0) {
+    await db.execAsync(schema);
+  } else {
+    await db.execAsync(`PRAGMA user_version = ${user_version};`);
+  }
+}
