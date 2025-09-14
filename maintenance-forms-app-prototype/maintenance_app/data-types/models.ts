9f15b943a5ca7e28cdad68ff2cdca3920a1e98be
Lukasz <lukabrzo@gmail.com>
2025-09-14 14:22:45 +0100

    Refactored the database migration file to create updated SQLite database schema Inspection interface to rename inspectorId to engineerId for clarity; updated Item interface to rename description to itemDescription for consistency; modified migration script to enhance database schema with improved foreign key references and added new tables for results, readings, and attachments.

--- maintenance-forms-app-prototype/maintenance_app/data-types/models.ts
+++ maintenance-forms-app-prototype/maintenance_app/data-types/models.ts
@@ -36,7 +36,7 @@ export interface Inspection {
   inspectionDate: string;
   inspectionCategory: InspectionCategory;
   itemId: number; // <-- Foreign Key to item
-  inspectorId: number; // <-- Foreign Key to user
+  engineerId: number; // <-- Foreign Key to user
   siteId: number; // <-- Foreign Key to site
   zoneId: number; // <-- Foreign Key to zone
   subchecks: Subcheck[]; // Array of subchecks for each inspection
@@ -61,7 +61,7 @@ export interface Item {
   itemId: number;
   itemType: string; // "Emergency Lighting" | "Die-Cut" etc.
   itemName: string; // "EL-01" | "Bobst DC-01"
-  description: string;
+  itemDescription: string;
   zoneId: number; // <-- Foreign Key to zone
 }
 
